9ac1c2261cf0a939a87fb1672a84cc8ca5830a68
Gavon Trubee <trubeeg@findlay.edu>
2024-02-15 12:06:00 -0500

    Added images and home text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,10 @@ import './homePage.css'
 export default function Home() {
   return (
     <div>
-      <h2>Home Page (test for GitHub)</h2>
+      <h2>Millstream Woodcarvers Club</h2>
+      <div class="textArea">
+        <p class="homeText">The club welcomes of youth and adults from all walks of life from beginners to experienced carvers who desire to learn more about wood carving and burning. Our purpose is to promote wood carving and to share ideas and techniques of wood carving in Northwestern Ohio. The club offers a carving resource center for classes, power equipment, tools, wood supply, a resource library and weekly fellowship among members at our clubhouse at open carving sessions and scheduled events. The clubhouse is located at 2230 Blanchard Avenue in Findlay, Ohio </p>
+      </div>
     </div>
   )
 }
