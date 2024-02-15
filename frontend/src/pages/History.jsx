fbef855af31f3aa397e6902e4cd5d2e254500530
Christian Palushaj <palushajc@findlay.edu>
2024-02-15 11:36:12 -0500

    Added paragraph

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -4,7 +4,9 @@ import './historyPage.css'
 export default function History() {
   return (
     <div className='history-container'>
-      <h2>History of the Club</h2>
+      <h1>History of Millstream Woodcarvers</h1>
+
+      <p>The Millstream Woodcarvers Club owes its existence to the imagination of our founder, Thomas O. Miller.</p>
     </div>
   )
 }
