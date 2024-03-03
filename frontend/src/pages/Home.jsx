1cc11cce42bc6488d767f7bd6ad3ea4925120b78
Gavon Trubee <trubeeg@findlay.edu>
2024-03-03 17:57:56 -0500

    Some changes to the design of the home page. Added a maps link

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,18 +4,19 @@ import './homePage.css';
 export default function Home() {
   return (
     <>
-      <div className='home-page-container'>
-      <h2>Millstream Woodcarvers Club</h2>
-      <div className="content">
-        <p>The club welcomes of youth and adults from all walks of life from beginners 
+      <div className='home-text'>
+        <h3 className="welcome">Welcome To</h3>
+        <h1>Millstream</h1>
+        <h3 className="club">Woodcarvers Club!</h3>
+        <p>We welcome youth and adults from all walks of life from beginners 
         to experienced carvers who desire to learn more about wood carving and burning. 
         Our purpose is to promote wood carving and to share ideas and techniques of wood carving 
         in Northwestern Ohio. The club offers a carving resource center for classes, power equipment, 
         tools, wood supply, a resource library and weekly fellowship among members 
         at our clubhouse at open carving sessions and scheduled events. 
-        The clubhouse is located at 2230 Blanchard Avenue in Findlay, Ohio. </p>
+        The clubhouse is located at <a href="http://maps.apple.com/?q=2230+Blanchard+Ave,+Findlay+OH">2230 Blanchard Avenue in Findlay, Ohio.</a>
+        </p>
       </div>
-    </div>
     <footer>
         <p>Millstream Woodcarvers Club</p>
         <p>2230 Blanchard Avenue, Findlay, Ohio</p>
