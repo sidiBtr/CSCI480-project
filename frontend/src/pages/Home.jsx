fbb3408d3dcafc42c4d0b79d21c6839c23da9667
Gavon Trubee <trubeeg@findlay.edu>
2024-03-14 09:45:36 -0400

    New home page changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,9 @@ import image110 from '../images/110.JPG'
 export default function Home() {
   return (
     <>
-      <div className='home-text'>
+    <section id="welcome-section">
+    <div className="img-text">
+      <div className="home-text">
         <h3 className="welcome">Welcome To</h3>
         <h1>Millstream</h1>
         <h3 className="club">Woodcarvers Club!</h3>
@@ -21,8 +23,12 @@ export default function Home() {
         at our clubhouse at open carving sessions and scheduled events. 
         The clubhouse is located at <Link to={"http://maps.apple.com/?q=2230+Blanchard+Ave,+Findlay+OH"}>2230 Blanchard Avenue in Findlay, Ohio.</Link>
         </p>
+        </div>
+        <div className="home-image">
+        <img src={image110} />
+        </div>
       </div>
-      
+    </section>
     <footer>
         <p>Millstream Woodcarvers Club</p>
         <p>2230 Blanchard Avenue, Findlay, Ohio</p>
