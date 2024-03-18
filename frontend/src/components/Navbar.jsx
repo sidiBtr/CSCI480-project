ca31930d7e95055900f29b038396f021988ac3da
Gavon Trubee <trubeeg@findlay.edu>
2024-03-17 20:36:11 -0400

    Home photo gallery

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import  './NavBarStyle.css'
+import logo from '../images/Untitled.svg'
 
 export default function Navbar() {
   return (
       <nav className='navbar'>
         <ul>
             <Link to={'/'}>
-            <li>Home</li>
+            <li><img src={logo} /></li>
             </Link>
             <Link to={'/events'}>
             <li>Events</li>
