032521f544174b1877845d65bcfcdec3db6ecdc4
Sidi Mohamed <bettars@findlay.edu>
2024-04-04 11:20:52 -0400

    commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cors())
 app.use(
     cors({
-        origin: 'https://mswoodcarving.onrender.com',
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
