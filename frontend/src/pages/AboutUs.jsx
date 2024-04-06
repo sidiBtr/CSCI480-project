31524546dc1a2d8663ff77249f2d94d94c601e2b
Christian Palushaj <palushajc@findlay.edu>
2024-04-05 22:43:19 -0400

    Typos

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -8,14 +8,14 @@ export default function AboutUs() {
       <h2 className='history-title'>History</h2>
          <p>The Millstream Wood Carvers owes its existence to the imagination of our founder, Thomas O. Miller.</p>
          <p>
-         Tom was introudced to wood carving in 1983 through a course taught at Findlay High School. He was so moved by the experience
-         that he decided to surround himself wih fellow carvers who shared his interests. Through his tireless efforts, the organization was 
+         Tom was introduced to wood carving in 1983 through a course taught at Findlay High School. He was so moved by the experience
+         that he decided to surround himself with fellow carvers who shared his interests. Through his tireless efforts, the organization was 
          inaugurated in August 1985 with 23 charter members. Monthly meetings were being held at various locations until Mr. Miller 
          donated his in-laws' estate to Millstream Wood Carvers in 1997, along with a generous contribution to a trust fund to meet
          future expenses.
          </p>
          <p>
-          The 'Miller House' which served as a tavern during the time of Findlay's Gas Boom era, was in rather decreipt state, not very
+          The 'Miller House' which served as a tavern during the time of Findlay's Gas Boom era, was in rather decrepit state, not very
           suitable for meetings. To satisfy the organization's needs, a two-car garage was erected on the property, half of which was dedicated
           to the storage of various equipment, while the other section was designed as a meeting place for the members. The growth of
           the organization soon exceeded the available space, and the entire building was converted to serve as a carving/meeting place with the 
@@ -29,7 +29,7 @@ export default function AboutUs() {
          <p>
           It is through the original generosity of Mr. Thomas O. Miller and the subsequent unselfish contributions of its members that 
           has allowed the Millstream Wood Carvers to continue to flourish in a spacious and comfortable environment,
-          dedicated to share their enthusiasm for wood carving to anyone who is interested.
+          dedicated to sharing their enthusiasm for wood carving with anyone who is interested.
          </p>
         </div>
     </div>
